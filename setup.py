a5109041cefb1cfb123dc0ec634f52fabdde1360
Evan Pratten <ewpratten@gmail.com>
2020-09-10 13:33:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='webapputils',
-      version='0.1',
+      version='0.2',
       description='Utils for webapps',
       url='https://github.com/Ewpratten/webapputils',
       author='Evan Pratten',
