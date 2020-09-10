2eeb9bef2f51245c34a4a49896fe86ad04786586
Evan Pratten <ewpratten@gmail.com>
2020-09-10 13:15:30 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='webapputils',
+      version='0.1',
+      description='Utils for webapps',
+      url='https://github.com/Ewpratten/webapputils',
+      author='Evan Pratten',
+      author_email='ewpratten@gmail.com',
+      license='MIT',
+      packages=['webapputils'],
+      zip_safe=False)
